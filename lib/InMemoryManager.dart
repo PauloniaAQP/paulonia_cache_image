b983d0242651f91564513dd037f2cd26e04a0425
Christofer Chavez <xnpiochv@gmail.com>
2020-12-09 19:06:46 -0500

    Added functionality of maxInMemoryImages and one bug fixed with PChacheImage init

--- lib/InMemoryManager.dart
+++ lib/InMemoryManager.dart
@@ -5,9 +5,19 @@ import 'package:paulonia_cache_image/paulonia_cache_image.dart';
 import 'package:paulonia_cache_image/paulonia_cache_image_mobile.dart'
     if (dart.library.html) 'package:paulonia_cache_image/paulonia_cache_image_web.dart';
 
+import 'constants.dart';
+
 class InMemoryManager {
   /// Stores the images in memory
   static HashMap<String, ImageStreamCompleter> _manager = HashMap();
+  static List<String> _savedImages;
+  static int _maxInMemoryImages;
+
+  /// Initialize the the In-memory manager
+  static void init({int maxInMemoryImages = Constants.INFINITE_IN_MEMORY_IMAGES}){
+    _maxInMemoryImages = maxInMemoryImages;
+    _savedImages = List();
+  }
 
   /// Get the image
   ///
@@ -15,7 +25,7 @@ class InMemoryManager {
   /// [getImage()] from the service of the platform.
   static ImageStreamCompleter getImage(PCacheImage key) {
     if (_manager.containsKey(key.url)) return _manager[key.url];
-    _manager[key.url] = MultiFrameImageStreamCompleter(
+    ImageStreamCompleter res = MultiFrameImageStreamCompleter(
       codec: PCacheImageService.getImage(
         key.url,
         key.retryDuration,
@@ -24,6 +34,12 @@ class InMemoryManager {
       ),
       scale: key.imageScale,
     );
-    return _manager[key.url];
+    if(_maxInMemoryImages != 0 && _savedImages.length == _maxInMemoryImages){
+      String removedUrl = _savedImages.removeAt(0);
+      _manager.remove(removedUrl);
+    }
+    _savedImages.add(key.url);
+    _manager[key.url] = res;
+    return res;
   }
 }
