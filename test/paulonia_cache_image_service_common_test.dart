a5de53afcfcd19522d1ab8addfe7055d0182a061
XnpioChV <xnpiochv@gmail.com>
2021-12-08 11:29:29 -0500

    Tests added

---
+++ test/paulonia_cache_image_service_common_test.dart
@@ -0,0 +1,95 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:paulonia_cache_image/paulonia_cache_image.dart';
+import 'package:paulonia_cache_image/paulonia_cache_image_mobile.dart'
+if (dart.library.html) 'package:paulonia_cache_image/paulonia_cache_image_web.dart';
+
+void main() {
+
+  group('Service functions:', () {
+    List<String> validUrls = [
+      'https://i.imgur.com/jhRBVEp.jpg',
+      'https://i.imgur.com/5RhnXjE.jpg',
+      'https://i.imgur.com/inAkwKw.jpg',
+    ];
+
+    test('downloadImage()', () async{
+      String invalidUrl = 'https://i.imgur.com/in.jpg';
+
+      for (String testUrl in validUrls) {
+        var bytes = await PCacheImageService.downloadImage(
+          testUrl,
+          Duration(seconds: 1),
+          Duration(seconds: 3),
+        );
+        expect(bytes.lengthInBytes != 0, isTrue);
+      }
+
+      var bytes = await PCacheImageService.downloadImage(
+        invalidUrl,
+        Duration(seconds: 1),
+        Duration(seconds: 3),
+      );
+      expect(bytes.lengthInBytes, equals(0));
+    });
+
+    test('getImage()', () async {
+      await PCacheImage.init();
+
+      expect(PCacheImageService.length, equals(0));
+      /// Without cache
+      await PCacheImageService.getImage(
+        validUrls.first,
+        Duration(seconds: 1),
+        Duration(seconds: 5),
+        false,
+      );
+      expect(PCacheImageService.length, equals(0));
+
+      /// Normal behaviour
+      for (int i = 0; i < validUrls.length; i++) {
+         var codec = await PCacheImageService.getImage(
+           validUrls[i],
+           Duration(seconds: 1),
+           Duration(seconds: 5),
+           true,
+         );
+         expect(codec.frameCount, equals(1));
+         expect(PCacheImageService.length, equals(i + 1));
+      }
+
+      /// Cached image
+      await PCacheImageService.getImage(
+        validUrls.first,
+        Duration(seconds: 1),
+        Duration(seconds: 5),
+        true,
+      );
+      expect(PCacheImageService.length, equals(validUrls.length));
+
+      /// Clear and not cached
+      await PCacheImageService.getImage(
+        validUrls.first,
+        Duration(seconds: 1),
+        Duration(seconds: 5),
+        false,
+        clearCacheImage: true,
+      );
+      expect(PCacheImageService.length, equals(validUrls.length - 1));
+
+      /// Clear and cached
+      await PCacheImageService.getImage(
+        validUrls.last,
+        Duration(seconds: 1),
+        Duration(seconds: 5),
+        true,
+        clearCacheImage: true,
+      );
+      expect(PCacheImageService.length, equals(validUrls.length - 1));
+
+      /// To clear the cache for other tests
+      await PCacheImageService.clearAllImages();
+    });
+
+  });
+
+}
--- test/paulonia_cache_image_test.dart
+++ test/paulonia_cache_image_test.dart
@@ -1,8 +1,55 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:hive/hive.dart';
+import 'package:paulonia_cache_image/InMemoryManager.dart';
+import 'package:paulonia_cache_image/constants.dart';
+import 'package:paulonia_cache_image/global_values.dart';
+import 'package:paulonia_cache_image/paulonia_cache_image.dart';
 
 void main() {
-  test('Nothing to test', () {
-    assert(1 == 1, true);
+
+  group('Initialize package', () {
+    test('with default values', () async {
+      await PCacheImage.init();
+      expect(GlobalValues.globalImageScale, Constants.DEFAULT_IMAGE_SCALE);
+      expect(GlobalValues.globalEnableCacheValue, Constants.DEFAULT_ENABLE_CACHE_VALUE);
+      expect(GlobalValues.globalRetryDuration, Constants.DEFAULT_RETRY_DURATION);
+      expect(GlobalValues.globalMaxRetryDuration, Constants.DEFAULT_MAX_RETRY_DURATION);
+      expect(GlobalValues.globalInMemoryValue, Constants.DEFAULT_IN_MEMORY_VALUE);
+      expect(InMemoryManager.maxInMemoryImages, Constants.DEFAULT_IN_MEMORY_IMAGES);
+      if (kIsWeb) {
+        expect(Hive.isBoxOpen(Constants.HIVE_CACHE_IMAGE_BOX), isTrue);
+        expect(Hive.isAdapterRegistered(17), isTrue);
+      }
+    });
+
+    test('with custom values', () async {
+      double imageScale = 3.0;
+      bool enableCache = false;
+      int retryDuration = 2;
+      int maxRetryDuration = 20;
+      bool enableInMemory = false;
+      int maxInMemoryImages = 4;
+
+      await PCacheImage.init(
+        imageScale: imageScale,
+        enableCache: enableCache,
+        retryDuration: retryDuration,
+        maxRetryDuration: maxRetryDuration,
+        enableInMemory: enableInMemory,
+        maxInMemoryImages: maxInMemoryImages,
+      );
+
+      expect(GlobalValues.globalImageScale, imageScale);
+      expect(GlobalValues.globalEnableCacheValue, enableCache);
+      expect(GlobalValues.globalRetryDuration, retryDuration);
+      expect(GlobalValues.globalMaxRetryDuration, maxRetryDuration);
+      expect(GlobalValues.globalInMemoryValue, enableInMemory);
+      expect(InMemoryManager.maxInMemoryImages, maxInMemoryImages);
+      if (kIsWeb) {
+        expect(Hive.isBoxOpen(Constants.HIVE_CACHE_IMAGE_BOX), isTrue);
+        expect(Hive.isAdapterRegistered(17), isTrue);
+      }
+    });
   });
 }
