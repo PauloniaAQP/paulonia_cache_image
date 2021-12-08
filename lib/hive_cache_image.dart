a42204e738efff7cd02a86f033287594de750d1e
XnpioChV <xnpiochv@gmail.com>
2021-12-08 11:29:05 -0500

    Multiple changes added: - Hive adapter id on the constants - Clear All images function added on InMemoryManager.dart. - Fixed bug on clear single image on InMemoryManager.dart - Refactoring clearALlImages function in paulonia_cache_image_mobile.dart - Fixed some errors on the downloadImage function - Dependencies updated

--- lib/hive_cache_image.dart
+++ lib/hive_cache_image.dart
@@ -1,10 +1,11 @@
 import 'dart:typed_data';
 
 import 'package:hive/hive.dart';
+import 'package:paulonia_cache_image/constants.dart';
 
 part 'hive_cache_image.g.dart';
 
-@HiveType(typeId: 17)
+@HiveType(typeId: Constants.HIVE_ADAPTER_ID)
 class HiveCacheImage {
   @HiveField(0)
   String url;
