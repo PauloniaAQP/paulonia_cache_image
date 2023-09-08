99abe02d734ea892c60094d96ca8b0157e15d343
Juan Leon <jjlc1997@live.com>
2023-09-08 17:26:31 -0500

    chore: change linter and fix warnings

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,55 +4,45 @@ import 'package:paulonia_cache_image/paulonia_cache_image.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await PCacheImage.init();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
+/// {@template my_app}
+/// My app
+/// {@endtemplate}
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  /// {@macro my_app}
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Paulonia Cache Image',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Paulonia Cache Image'),
+      home: const MyHomePage(title: 'Paulonia Cache Image'),
     );
   }
 }
 
+/// {@template my_home_page}
+/// My home page
+/// {@endtemplate}
 class MyHomePage extends StatefulWidget {
-  MyHomePage({super.key, required this.title});
+  /// {@macro my_home_page}
+  const MyHomePage({required this.title, super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  /// The title of the page
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  @override
   void initState() {
     super.initState();
   }
@@ -64,37 +54,40 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.white,
         title: Text(
           widget.title,
-          style: TextStyle(color: Colors.black),
+          style: const TextStyle(color: Colors.black),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-          child: Icon(
-            Icons.refresh,
-            color: Colors.black,
-          ),
-          backgroundColor: Colors.white,
-          onPressed: () {
-            setState(() {});
-          }),
+        backgroundColor: Colors.white,
+        onPressed: () {
+          setState(() {});
+        },
+        child: const Icon(
+          Icons.refresh,
+          color: Colors.black,
+        ),
+      ),
       body: ListView(
         children: [
-          ListTile(
-            title: Text("In memory cached network image"),
+          const ListTile(
+            title: Text('In memory cached network image'),
           ),
-          Container(
+          SizedBox(
             height: 250,
             child: Card(
               elevation: 10,
               child: Image(
-                image: PCacheImage('https://i.imgur.com/jhRBVEp.jpg',
-                    enableInMemory: true),
+                image: PCacheImage(
+                  'https://i.imgur.com/jhRBVEp.jpg',
+                  enableInMemory: true,
+                ),
               ),
             ),
           ),
-          ListTile(
-            title: Text("Storage cached network image"),
+          const ListTile(
+            title: Text('Storage cached network image'),
           ),
-          Container(
+          SizedBox(
             height: 250,
             child: Card(
               elevation: 10,
@@ -105,16 +98,18 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
           ),
-          ListTile(
-            title: Text("Not cached image"),
+          const ListTile(
+            title: Text('Not cached image'),
           ),
-          Container(
+          SizedBox(
             height: 250,
             child: Card(
               elevation: 10,
               child: Image(
-                image: PCacheImage('https://i.imgur.com/inAkwKw.jpg',
-                    enableCache: false),
+                image: PCacheImage(
+                  'https://i.imgur.com/inAkwKw.jpg',
+                  enableCache: false,
+                ),
               ),
             ),
           ),
