44a364ad31ed0902caa5d6ca2530d04488568886
XnpioChV <xnpiochv@gmail.com>
2023-10-14 13:36:38 -0500

    test: Fix tests

--- lib/paulonia_cache_image_mobile.dart
+++ lib/paulonia_cache_image_mobile.dart
@@ -151,7 +151,8 @@ class PCacheImageService {
           '1NzMxNDI2*_ga_CW55HF8NVT*MTY5NzMwMzY3My4zMTYuMS4xNjk3MzA2MzYzLjUy'
           'LjAuMA..';
     }
-    // coverage:ignore-line
-    return FirebaseStorage.instance.refFromURL(gsUrl).getDownloadURL();
+    return FirebaseStorage.instance // coverage:ignore-line
+        .refFromURL(gsUrl)
+        .getDownloadURL();
   }
 }
